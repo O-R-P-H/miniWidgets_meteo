cf7e3dc99ecbcd2e956fe8fde5bf665a1f621c31
ORPH(desktop) <hex2152@gmail.com>
2025-03-06 01:43:29 +0300

    init

---
+++ electron/main.cjs
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+app.whenReady().then(() => {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: false,
+        }
+    });
+
+    const isDev = !app.isPackaged;
+
+    if (isDev) {
+        mainWindow.webContents.openDevTools()
+        mainWindow.loadURL('http://localhost:5173');
+    } else {
+        const indexPath = path.join(__dirname, '..', 'dist', 'index.html');
+        console.log('Loading File:', indexPath);
+        mainWindow.webContents.openDevTools()
+        mainWindow.loadFile(indexPath);
+    }
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') app.quit();
+    });
+});
